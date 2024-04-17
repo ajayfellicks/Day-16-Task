dbc85b6dd6662372908dd1c9241600e07d87457a
ajayfellicks <ajaykumar20002509@gmail.com>
2024-04-17 17:35:59 +0530

    completed

---
+++ script.js
@@ -0,0 +1,34 @@
+setTimeout(() => {
+  document.getElementById("text").innerHTML = "10";
+  setTimeout(() => {
+    document.getElementById("text").innerHTML = "9";
+    setTimeout(() => {
+      document.getElementById("text").innerHTML = "8";
+      setTimeout(() => {
+        document.getElementById("text").innerHTML = "7";
+        setTimeout(() => {
+          document.getElementById("text").innerHTML = "6";
+          setTimeout(() => {
+            document.getElementById("text").innerHTML = "5";
+            setTimeout(() => {
+              document.getElementById("text").innerHTML = "4";
+              setTimeout(() => {
+                document.getElementById("text").innerHTML = "3";
+                setTimeout(() => {
+                  document.getElementById("text").innerHTML = "2";
+                  setTimeout(() => {
+                    document.getElementById("text").innerHTML = "1";
+                    setTimeout(() => {
+                      document.getElementById("text").innerHTML =
+                        "Happy Independence Day";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+});
